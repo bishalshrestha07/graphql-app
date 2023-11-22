c9f936f63a455458726597cec0b0cb97217311cf
bishalshrestha13 <biscro7@gmail.com>
2023-11-22 07:56:10 +0545

    feat: add prisma to connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -14,10 +15,41 @@ async function init() {
         type Query {
             hello: String
         }
+
+        type Mutation {
+            createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+        }
     `,
     resolvers: {
       Query: {
         hello: () => "world, I am bishal",
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random-salt",
+            },
+          });
+          return true;
+        },
       },
     },
   });
